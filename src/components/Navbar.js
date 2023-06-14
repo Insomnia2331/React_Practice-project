14f9eb0663004a42895880b36271d02716d9e906
Insomnia2331 <103230242+Insomnia2331@users.noreply.github.com>
2023-06-14 20:27:31 +1200

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Icon from "../images/react-icon-large.png"
+
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={Icon} className="nav--icon" alt="imagehaha" />
+
+            <></>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
